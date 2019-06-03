1e9d8764ab0e9a88d4a6ab642ea7b58cb44b6121
zhoudaofan <daofan.zhou@ximalaya.com>
2019-06-03 15:40:02 +0800

    update

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './home.scss';
+
+export default class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        <p className="home">this is home page</p>
+      </div>
+    )
+  }
+}
