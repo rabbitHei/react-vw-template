fd7c42b6c63ed5e99d4abe5d75b55c5aee667f5c
zhoudaofan <daofan.zhou@ximalaya.com>
2019-08-27 17:35:40 +0800

    update sdk config

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 import './index.scss';
 import './utils/axiosMiddleware';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import '@/utils/sdkConfig';//sdk
+
+// import VConsole from 'vconsole';
+// const vconsole = new VConsole();
 
 ReactDOM.render(
-  <Router basename="/">
-    <Routes />
-  </Router>,
+  <Routes />,
   document.getElementById('root')
 );
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './home.scss';
-
+import appImgs from '@/constant/appImgs.js'
 export default class Home extends Component {
 
   render() {
     return (
       <div>
+        <img src={appImgs.back} alt="" />
         <p className="home">this is home page</p>
       </div>
     )
